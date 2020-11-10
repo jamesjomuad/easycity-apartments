6f7745fed9268145d3f716cef2cac3099f92ec2c
Unknown <jomuad_james@yahoo.com>
2020-11-10 08:19:20 +0800

    add composer

--- easycity-apartments.php
+++ easycity-apartments.php
@@ -16,21 +16,12 @@ if( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 define('EASYCITY_DIR', dirname(__FILE__) );
 define('EASYCITY_URL', plugin_dir_url( __FILE__ ) );
 
-// $basename = plugin_basename( __FILE__ );
-// $path = plugin_dir_path( __FILE__ );
-// $url = plugin_dir_url( __FILE__ );
-// $slug = dirname($basename);
-
-include_once('inc/class-easycity.php');
-include_once('inc/helpers.php');
-include_once('inc/actions.php');
-include_once('inc/filters.php');
-include_once('inc/shortcodes.php');
-include_once('inc/ajax.php');
-
+if (file_exists(dirname(__FILE__) . '/vendor/autoload.php')) {
+    require_once dirname(__FILE__) . '/vendor/autoload.php';
+}
 
 // add_action('init', function(){
 //     dd(
-//         easycity()->view('map-popup')
+//         // new \Inc
 //     );
 // });
